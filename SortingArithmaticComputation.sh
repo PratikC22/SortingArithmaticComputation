15d9de12d03fe2013543c7f2fb38cbd296abcf27
Pratik Chaudhari <pratikc1020@gmail.com>
2021-05-31 18:41:45 +0530

    SpellCheck

--- SortingArithmaticComputation.sh
+++ SortingArithmaticComputation.sh
@@ -1,23 +1,23 @@
 #! /bin/bash -x
 
-declare -A Results	#Declaring Associative array/Dictionary
+declare -A Results	#Declaring Dictionary
 
 read -p "Enter 3 inputs a, b and c : " a b c;	#Taking 3 inputs
 
 
 #Conditions 1 to 4:
-condition1=$(( ( $b * $c ) + $a ));
-condition2=$(( ( $a * $b ) + $c ));
-condition3=$(( ( $a / $b ) + $c ));
-condition4=$(( ( $a % $b ) + $c ));
+condition1=$(( ($b*$c)+$a ));
+condition2=$(( ($a*$b)+$c ));
+condition3=$(( ($a/$b)+$c ));
+condition4=$(( ($a%$b)+$c ));
 
-#Storing results in an associative array:
+#Storing results in Dictionary:
 Results["condition1"]=$condition1
 Results["condition2"]=$condition2
 Results["condition3"]=$condition3
 Results["condition4"]=$condition4
 
-#Displaying results from associative array:
+#Displaying results from Dictionary:
 echo "$a+$b*$c = ${Results[condition1]}";
 echo "$a*$b+$c = ${Results[condition2]}";
 echo "$c+$a/$b = ${Results[condition3]}";
