a2aa63a2e4b1949e2a1d978d55a1d3e469c875fe
Pratik Chaudhari <pratikc1020@gmail.com>
2021-05-31 17:18:15 +0530

    Added feature to store result in an associative array

--- SortingArithmaticComputation.sh
+++ SortingArithmaticComputation.sh
@@ -1,13 +1,25 @@
 #! /bin/bash -x
 
-read -p "Enter 3 inputs a, b and c : " a b c;
+declare -A Results	#Declaring Associative array/Dictionary
 
+read -p "Enter 3 inputs a, b and c : " a b c;	#Taking 3 inputs
+
+
+#Conditions 1 to 4:
 condition1=$(( ( $b * $c ) + $a ));
 condition2=$(( ( $a * $b ) + $c ));
 condition3=$(( ( $a / $b ) + $c ));
 condition4=$(( ( $a % $b ) + $c ));
 
+#Storing results in an associative array:
+Results["condition1"]=$condition1
+Results["condition2"]=$condition2
+Results["condition3"]=$condition3
+Results["condition4"]=$condition4
+
+#Displaying results:
 echo "$a+$b*$c = $condition1";
 echo "$a*$b+$c = $condition2";
 echo "$c+$a/$b = $condition3";
 echo "$a%$b+$c = $condition4";
+
