ce8a59b661c11bd1ac4b99a519d5243c679f0aa0
Pratik Chaudhari <pratikc1020@gmail.com>
2021-05-31 15:22:19 +0530

    Added compute feature 4

--- SortingArithmaticComputation.sh
+++ SortingArithmaticComputation.sh
@@ -5,7 +5,9 @@ read -p "Enter 3 inputs a, b and c : " a b c;
 condition1=$(( ( $b * $c ) + $a ));
 condition2=$(( ( $a * $b ) + $c ));
 condition3=$(( ( $a / $b ) + $c ));
+condition4=$(( ( $a % $b ) + $c ));
 
 echo "$a+$b*$c = $condition1";
 echo "$a*$b+$c = $condition2";
 echo "$c+$a/$b = $condition3";
+echo "$a%$b+$c = $condition4";
