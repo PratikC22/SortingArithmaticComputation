a844476f1087f35e32242efe9afc835685ae7a22
Pratik Chaudhari <pratikc1020@gmail.com>
2021-06-06 09:45:28 +0530

    Added UC-7-DictToArray

--- SortingArithmaticComputation.sh
+++ SortingArithmaticComputation.sh
@@ -1,25 +1,24 @@
 #! /bin/bash -x
 
-declare -A Results	#Declaring Associative array/Dictionary
 
 read -p "Enter 3 inputs a, b and c : " a b c;	#Taking 3 inputs
 
 
-#Conditions 1 to 4:
+#Conditions 1 to 4 :
 condition1=$(( ( $b * $c ) + $a ));
 condition2=$(( ( $a * $b ) + $c ));
 condition3=$(( ( $a / $b ) + $c ));
 condition4=$(( ( $a % $b ) + $c ));
 
-#Storing results in an associative array:
+#Storing results in a Dictionary :
 Results["condition1"]=$condition1
 Results["condition2"]=$condition2
 Results["condition3"]=$condition3
 Results["condition4"]=$condition4
 
-#Displaying results from associative array:
-echo "$a+$b*$c = ${Results[condition1]}";
-echo "$a*$b+$c = ${Results[condition2]}";
-echo "$c+$a/$b = ${Results[condition3]}";
-echo "$a%$b+$c = ${Results[condition4]}";
+#Displaying results from Dictionary :
+echo "Results of all computations : " ${Results[@]}
 
+#Dictionary to array :
+Array=(${Results[@]})
+echo ${Array[*]}
